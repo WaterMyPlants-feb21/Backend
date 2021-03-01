63237fffaeb5ff541386dde60cee996bee0afa73
Jamie <jamielynjenks@gmail.com>
2021-03-01 11:51:22 -0700

    created plant model

--- src/main/java/com/lambdaschool/watermyplants/models/Plant.java
+++ src/main/java/com/lambdaschool/watermyplants/models/Plant.java
@@ -1,4 +1,56 @@
 package com.lambdaschool.watermyplants.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="plants")
 public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long plantid;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String phonenumber;
+
+    public Plant() {
+    }
+
+    public long getPlantid() {
+        return plantid;
+    }
+
+    public void setPlantid(long plantid) {
+        this.plantid = plantid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 }
