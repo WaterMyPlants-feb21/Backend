5a0c7fcf9ecda07cd0e079f225d995f6c7467d62
Fidan Novruzova <yf@Fidans-MBP.localdomain>
2021-03-01 14:52:30 -0500

    user-controllers

--- src/main/java/com/lambdaschool/watermyplants/services/UserService.java
+++ src/main/java/com/lambdaschool/watermyplants/services/UserService.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface UserService {
     User save(User user);
-    User update(User user);
+    User update(User user,long id);
     User findUserById(long id);
 }
