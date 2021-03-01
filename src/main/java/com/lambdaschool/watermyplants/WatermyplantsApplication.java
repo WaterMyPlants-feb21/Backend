7946067a0ce93cc5492389509605ca5b3ea132aa
Jamie <jamielynjenks@gmail.com>
2021-03-01 11:01:02 -0700

    first commit

---
+++ src/main/java/com/lambdaschool/watermyplants/WatermyplantsApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.watermyplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermyplantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermyplantsApplication.class, args);
+    }
+
+}
