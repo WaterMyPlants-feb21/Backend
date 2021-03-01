3e39941d1d30a3f4a202275b7ca4874ed662b53e
Fidan Novruzova <yf@Fidans-MBP.localdomain>
2021-03-01 13:37:17 -0500

    user-model

--- src/main/java/com/lambdaschool/watermyplants/models/User.java
+++ src/main/java/com/lambdaschool/watermyplants/models/User.java
@@ -1,4 +1,75 @@
 package com.lambdaschool.watermyplants.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userid;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User()
+    {
+    }
+
+    public User(
+        String username,
+        String phoneNumber,
+        String password)
+    {
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public long getUserid()
+    {
+        return userid;
+    }
+
+    public void setUserid(long userid)
+    {
+        this.userid = userid;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
